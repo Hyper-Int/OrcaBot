ebe8f2a7756c065d7e48380abecee5a8e312b05d
Robert Macrae <30808597+robdmac@users.noreply.github.com>
2026-01-17 01:15:51 +0000

    Fix routes were not configured to run with the Edge Runtime (#14)

--- frontend/src/app/(app)/dashboards/page.tsx
+++ frontend/src/app/(app)/dashboards/page.tsx
@@ -1,7 +1,11 @@
 // Copyright 2026 Robert Macrae. All rights reserved.
 // SPDX-License-Identifier: LicenseRef-Proprietary
 
+"use client";
+
 import dynamic from "next/dynamic";
+
+export const runtime = "edge";
 
 const DashboardsPage = dynamic(() => import("./page.client"), {
   ssr: false,
